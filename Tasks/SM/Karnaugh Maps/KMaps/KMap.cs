0de89f8e214184f6ac5a47e99b350bea7ba13650
KOLESNiii <timkolesnichenko05@gmail.com>
2022-11-28 22:46:20 +0000

    Added Karnaugh Maps Decoder
    Added files

---
+++ Tasks/SM/Karnaugh Maps/KMaps/KMap.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace KMap
+{
+    public class Group
+    {
+        public Group(string value, bool temp = false)
+        {
+            Value = value;
+            Temp = temp;
+            NumOnes = value.Count(v => v.ToString() == "1");
+        }
+        public string Value;
+        bool Temp;
+        public int NumOnes;
+        public bool Merged = false;
+        public List<int> Minterms = new List<int>();
+
+        public static List<Group> MakeGroups(List<string> values)
+        {
+            List<Group> groups = new List<Group>();
+            for (int i = 0; i < values.Count(); i++)
+            {
+                groups.Add(new Group(values[i]));
+                groups[i].Minterms.Add(ConvertBinToDen(values[i]));
+            }
+            return groups;
+        }
+
+        public static int ConvertBinToDen(string binary)
+        {
+            int output = 0;
+            binary = Reverse(binary);
+            var binaryArray = binary.Select(s => int.Parse(s.ToString())).ToList();
+            for (int i = 0; i < binary.Length; i++)
+            {
+                output += binaryArray[i]*(int)(Math.Pow(2, i));
+            }
+            return output;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+    
+    public static class Simplifier
+    {
+        public static List<Group> Combine(List<Group> groups)
+        {
+            List<Group> newGroups = new List<Group>();
+            bool finished = false;
+            List<Group> finalGroups = new List<Group>();
+            while (!finished)
+            {
+                var originalNumGroups = groups.Count();
+                foreach (Group group in groups.OrderBy(g => g.NumOnes))
+                {
+                    foreach (Group possibleGroup in groups.Where(g => g.NumOnes == group.NumOnes + 1))
+                    {
+                        for (int i = 0; i < group.Value.Length; i++)
+                        {
+                            var tempString = ReplaceChar(group.Value, "-", i);
+                            var possibleTempString = ReplaceChar(possibleGroup.Value, "-", i);
+                            if (CompareStrings(tempString, possibleTempString))
+                            {
+                                if (newGroups.Where(g => g.Value == tempString).Count() == 0)
+                                {
+                                    newGroups.Add(new Group(tempString));
+                                }
+                                group.Merged = true;
+                                possibleGroup.Merged = true;
+                                foreach (Group workingGroup in newGroups.Where(g => g.Value == tempString))
+                                {
+                                    workingGroup.Minterms.AddRange(group.Minterms.Where(m => !workingGroup.Minterms.Contains(m)).ToList());
+                                    workingGroup.Minterms.AddRange(possibleGroup.Minterms.Where(m => !workingGroup.Minterms.Contains(m)).ToList());
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach (Group group in groups.Where(g => g.Merged == false))
+                {
+                    finalGroups.Add(group);
+                }
+                if (newGroups.Count() == 0)
+                {
+                    finished = true;
+                }
+                groups = newGroups.ToList();
+                newGroups.Clear();
+
+            }
+            return finalGroups;
+        }
+
+        private static string ReplaceChar(string sourceStr, string toReplace, int index)
+        {
+            sourceStr = sourceStr.Remove(index, 1);
+            sourceStr = sourceStr.Insert(index, toReplace);
+            return sourceStr;
+        }
+
+        private static bool CompareStrings(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static string GenerateOutputString(List<Group> groups, string letters)
+        {
+            List<string> ANDGroups = new List<string>();
+            foreach (Group g in groups)
+            {
+                ANDGroups.Add(GenerateANDGroup(g.Value, letters));
+            }
+            return string.Join(" + ", ANDGroups);
+        }
+
+        private static string GenerateANDGroup(string value, string letters)
+        {
+            string output = String.Empty;
+            for (int i = 0; i < value.Length; i++)
+            {
+                string c = value[i].ToString();
+                if (c == "-")
+                {
+                    continue;
+                }
+                output += letters[i];
+                if (c == "0")
+                {
+                    output += "'";
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ Tasks/SM/Karnaugh Maps/KMaps/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using KMap;
+
+namespace Program
+{
+    class ProgramMain
+    {
+        public static void Main(string[] args)
+        {
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            List<string> values = new List<string>{"0000", "0001", "0100", "0101", "1100", "1101", "1000", "1001", "0010", "0110", "1010"};            
+            if (!Validation.IsSameLength(values))
+            {
+                throw new Exception("All inputs must be the same number of bits");
+            }
+            else if (!Validation.IsWithinRange(values, letters.Length))
+            {
+                throw new Exception("Inputs must be smaller than number of letters denoting inputs");
+            }
+            
+            var finalGroups = new List<Group>();
+            var groups = Group.MakeGroups(values);
+            groups = Simplifier.Combine(groups);
+            foreach (Group g in groups)
+            {
+                Console.WriteLine(g.Value);
+            }
+            Console.WriteLine("\n");
+            List<int> allPrimeImplicants = new List<int>();
+            bool primeImplicantsLeft = true;
+            foreach (Group g in groups)
+                {
+                    allPrimeImplicants.AddRange(g.Minterms);
+                }
+            int essentialPrimeImplicant;
+            while (primeImplicantsLeft)
+            {
+                var essentialPrimeImplicants = allPrimeImplicants.GroupBy(i => i).Where(m => m.Count() == 1).Select(m => m.First()).ToList();
+                if (essentialPrimeImplicants.Count() == 0)
+                {
+                    primeImplicantsLeft = false;
+                    break;
+                }
+                else
+                {
+                    essentialPrimeImplicant = essentialPrimeImplicants[0];
+                }
+                foreach (Group g in groups)
+                {
+                    if (g.Minterms.Contains(essentialPrimeImplicant))
+                    {
+                        finalGroups.Add(g);
+                        groups.Remove(g);
+                        allPrimeImplicants = allPrimeImplicants.Where(m => !g.Minterms.Contains(m)).ToList();
+                        break;
+                    }
+                }
+            }
+            if (allPrimeImplicants.Count() > 0)
+            {
+                finalGroups.Add(groups[0]);
+            }
+            //allPrimeImplicants = allPrimeImplicants.Distinct().ToList();
+            foreach (Group g in finalGroups)
+            {
+                Console.WriteLine(g.Value);
+            }
+            Console.WriteLine(Simplifier.GenerateOutputString(finalGroups, letters));
+        }
+    }
+
+    static class Validation
+    {
+        public static bool IsSameLength(List<string> values)
+        {
+            var current = values[0];
+            foreach (string s in values)
+            {
+                if (s.Length != current.Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    current = s;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsWithinRange(List<string> values, int maxValue)
+        {
+            if (maxValue < values[0].Length)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
